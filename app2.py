5e43a80e9fa2c7a86d3b6f302e00be30a33980c1
panther72003@gmail.com <53586634+JoshuaFarmerATX@users.noreply.github.com>
2020-10-02 20:21:22 -0500

    Made changes to app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,24 @@
-print("Hey World")
+# # Pull a repo from the site
+# git clone path/to/repo
+
+# # Check Status
+# git status
+
+# # Add files to the commit
+# git add .
+
+# # git commit
+# git commit -m "some test"
+
+# # Push commit to the repo
+# git push
+
+
+
+dog_one = 5
+dog_two = 2
+
+if dog_one >= 3:
+    print("Good Dog")
+else:
+    print("Bad Dog")
